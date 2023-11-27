db640d60a5c51b8981438b9876798b96dd4feba5
Seungki <130020647+Seungkizz@users.noreply.github.com>
2023-11-27 19:51:46 +0900

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,40 @@
+const jsonData = {
+
+  "portfolioList": [
+    {
+      "id": 1,
+      "title": "React TODO LIST",
+      "image": "./imgs/리액트TODOLIST.png",
+      "description": "리액트 학습하며 제작한 페이지입니다. 할 일 추가, 삭제, 체크 기능이 가능합니다.",
+      "technologies": ["JSX", "Styled Components", "React Hooks"],
+      "liveLink": "https://seungkizz.github.io/react_todo_list/",
+      "githubLink": "https://github.com/Seungkizz/react_todo_list"
+    },
+    {
+      "id": 2,
+      "title": "Responsive Web",
+      "image": "./imgs/halo.png",
+      "description": "국비 교육과정에서 팀 프로젝트로 진행한 반응형 웹 페이지입니다.",
+      "technologies": ["HTML", "CSS", "JavaScript", "Media Query"],
+      "liveLink": "https://seungkizz.github.io/HaloCrystal/",
+      "githubLink": "https://github.com/Seungkizz/Seungkizz.github.io/tree/main/HaloCrystal"
+    },
+    {
+      "id": 3,
+      "title": "React OpenAPI 활용",
+      "image": "./imgs/pixabay.png",
+      "description": "리액트 학습하며 pixabayAPI를 이용하여 제작한 페이지입니다. 이미지를 검색하여 정보를 제공하고 해당 이미지 클릭 시 다운로드 할 수 있는 링크로 이동하게 됩니다.",
+      "technologies": ["JSX", "Styled Components", "React Hooks", "OpenAPI"],
+      "liveLink": "https://seungkizz.github.io/pixabay/",
+      "githubLink": "https://github.com/Seungkizz/pixabay"
+    },
+    {
+      "id": 4,
+      "title": "Spring / Oracle 기반 포토폴리오",
+      "image": "./imgs/pompom.png",
+      "description": "국비 교육과정에서 Spring과 Oracle 기반의 포토폴리오를 제작하였습니다. 기획안 작성부터 다양한 기능을 하나씩 추가해 나가면서 웹 프로그래밍의 기본기를 확립하고 웹 애플리케이션의 전 과정을 익혔습니다.",
+      "technologies": ["Spring Framework", "Oracle", "Maven", "Mybatis"],
+      "githubLink": "https://github.com/Seungkizz/pompom"
+    }
+  ]
+};
