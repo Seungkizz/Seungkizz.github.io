a7b8cc5d4eb82b1297e1027b92f39cbdf71700f1
Seungki <130020647+Seungkizz@users.noreply.github.com>
2023-12-06 18:13:17 +0900

    data.js 수정

--- js/data.js
+++ js/data.js
@@ -23,16 +23,16 @@ const jsonData = {
       "id": 3,
       "title": "React OpenAPI 활용",
       "image": "./imgs/pixabay.png",
-      "description": "리액트 학습하며 pixabayAPI를 이용하여 제작한 페이지입니다. 이미지를 검색하여 정보를 제공하고 해당 이미지 클릭 시 다운로드 할 수 있는 링크로 이동하게 됩니다.",
+      "description": "리액트 학습하며 pixabayAPI를 이용하여 제작한 페이지입니다. 검색을 통하여 정보를 제공하고 해당 이미지 클릭 시 다운로드 할 수 있는 링크로 이동하게 됩니다.",
       "technologies": ["JSX", "Styled Components", "React Hooks", "OpenAPI"],
       "liveLink": "https://seungkizz.github.io/pixabay/",
       "githubLink": "https://github.com/Seungkizz/pixabay"
     },
     {
       "id": 4,
-      "title": "Spring / Oracle 기반 포토폴리오",
+      "title": "Spring Framwork / Oracle 포토폴리오",
       "image": "./imgs/pompom.png",
-      "description": "국비 교육과정에서 Spring과 Oracle 기반의 포토폴리오를 제작하였습니다. 기획안 작성부터 다양한 기능을 하나씩 추가해 나가면서 웹 프로그래밍의 기본기를 확립하고 웹 애플리케이션의 전 과정을 익혔습니다.",
+      "description": "국비 교육과정에서 Spring과 Oracle 기반의 게시판 CRUD 포토폴리오를 제작하였습니다. 기획안 작성부터 다양한 기능을 하나씩 추가해 나가면서 웹 프로그래밍의 기본기를 확립하고 웹 애플리케이션의 전 과정을 익혔습니다.",
       "technologies": ["Spring Framework", "Oracle", "Maven", "Mybatis"],
       "githubLink": "https://github.com/Seungkizz/pompom"
     }
